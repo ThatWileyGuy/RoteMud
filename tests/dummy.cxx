1e68fc7ae07026fca965bdc43a6168389b8efe2c
Andrew Wiley <wiley@outlook.com>
2021-06-12 01:03:48 -0700

    serialization fun

--- tests/dummy.cxx
+++ tests/dummy.cxx
@@ -1,8 +1,43 @@
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include <sstream>
+#include <boost/archive/tmpdir.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
 
-BOOST_AUTO_TEST_CASE(free_test_function)
-/* Compare with void free_test_function() */
+using namespace boost::serialization;
+
+class SuperFun
 {
+    int m_x;
+    int m_y;
+    double m_wheee;
+
+    public:
+    SuperFun() : m_x(1), m_y(2), m_wheee(3.0)
+    {
+    }
+
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int)
+    {
+        ar & make_nvp("x", m_x);
+        ar & make_nvp("y", m_y);
+        ar & make_nvp("wheee", m_wheee);
+    }
+};
+
+BOOST_AUTO_TEST_CASE(super_fun_test)
+{
+    SuperFun s;
+
+    std::stringstream ss;
+
+    boost::archive::xml_oarchive out(ss);
+    out << make_nvp("SuperFun", s);
+
+    std::cout << ss.str() << std::endl;
+
     BOOST_TEST(true /* test assertion */);
 }
